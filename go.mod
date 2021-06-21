94d912e09e086621b8d3d14636c5d4189ae92694
Fabio Hiroki <fabiothiroki@gmail.com>
2021-06-21 16:23:03 -0300

    setup credentials

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/fabiothiroki/go-twitter-bot
 
 go 1.15
 
-require github.com/dghubble/go-twitter v0.0.0-20210609183100-2fdbf421508e // indirect
+require (
+	github.com/dghubble/go-twitter v0.0.0-20210609183100-2fdbf421508e
+	github.com/dghubble/oauth1 v0.7.0
+	github.com/joho/godotenv v1.3.0
+)
--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	creds := getCreds()
+	client := getClient(creds)
+
+	tweet, resp, err := client.Statuses.Update("just setting up my twttr", nil)
+
+	if err != nil {
+		log.Fatal("Error publishing tweet", err)
+	}
+
+	fmt.Println("Hello World! ", resp, tweet)
+}
+
+func getClient(creds *creds) *twitter.Client {
+	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
+	token := oauth1.NewToken(creds.AccessToken, creds.AccessSecret)
+
+	httpClient := config.Client(oauth1.NoContext, token)
+
+	return twitter.NewClient(httpClient)
 }
