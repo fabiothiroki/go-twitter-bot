1bc8674fc74506e44b348e8f0addd0237b28149c
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-06-22 12:12:50 -0300

    extract to separate file (#5)

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/dghubble/go-twitter/twitter"
-	"github.com/dghubble/oauth1"
 	"github.com/joho/godotenv"
 )
 
@@ -16,24 +14,7 @@ func main() {
 		log.Println("Error loading .env file")
 	}
 
-	creds := getCreds()
-	client := getClient(creds)
-
 	quote := GetLeastRecentPostedQuote()
-	_, resp, err := client.Statuses.Update(quote.text+" — "+quote.author, nil)
 
-	if err != nil {
-		log.Fatal("Error publishing tweet", err)
-	}
-
-	log.Println("Post tweet response", resp.Status)
-}
-
-func getClient(creds *creds) *twitter.Client {
-	config := oauth1.NewConfig(creds.ConsumerKey, creds.ConsumerSecret)
-	token := oauth1.NewToken(creds.AccessToken, creds.AccessSecret)
-
-	httpClient := config.Client(oauth1.NoContext, token)
-
-	return twitter.NewClient(httpClient)
+	PostTweetStatusUpdate(quote.text + " — " + quote.author)
 }
