8cc0d99fc12397d00d4cd2090e82b981f65dea48
Fabio Hiroki <fabiothiroki@gmail.com>
2021-06-30 07:58:05 -0300

    add simple test

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -1,13 +1,25 @@
 package database
 
 import (
+	"context"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/pashagolub/pgxmock"
 )
 
-func TestSomething(t *testing.T) {
+func TestCloseConnectionAfterFetchingQuote(t *testing.T) {
+	mockConn, err := pgxmock.NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockConn.Close(context.Background())
 
-	assert.True(t, true, "True is true!")
+	mockConn.ExpectClose()
+
+	GetLeastRecentPostedQuote(mockConn)
+
+	if err := mockConn.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 
 }
