50c68a47846e63a1e4b65a1e33dcac14c7c49856
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-06-30 16:40:27 -0300

    improve tests (#21)

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -5,21 +5,30 @@ import (
 	"testing"
 
 	"github.com/pashagolub/pgxmock"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestCloseConnectionAfterFetchingQuote(t *testing.T) {
+func TestGetLeastRecentPostedQuote(t *testing.T) {
 	mockConn, err := pgxmock.NewConn()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
 	defer mockConn.Close(context.Background())
 
+	rows := pgxmock.NewRows([]string{"id", "text", "author"}).
+		AddRow(1, "tweet quote", "Fabio")
+
+	mockConn.
+		ExpectQuery("select id, text, author from quotes order by last_posted_at ASC LIMIT 1").
+		WillReturnRows(rows)
+
 	mockConn.ExpectClose()
 
-	GetLeastRecentPostedQuote(mockConn)
+	result := GetLeastRecentPostedQuote(mockConn)
 
 	if err := mockConn.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 
+	assert.Equal(t, result, &Quote{ID: 1, Text: "tweet quote", Author: "Fabio"})
 }
