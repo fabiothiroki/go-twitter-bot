acd92c04278f9fc0ca63292d1eabc360f9960783
Fabio Hiroki <fabio.hiroki@quintoandar.com.br>
2021-07-01 15:23:17 -0300

    add tests for update (#22)

--- internal/database/database_test.go
+++ internal/database/database_test.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/pashagolub/pgxmock"
 	"github.com/stretchr/testify/assert"
@@ -22,13 +23,33 @@ func TestGetLeastRecentPostedQuote(t *testing.T) {
 		ExpectQuery("select id, text, author from quotes order by last_posted_at ASC LIMIT 1").
 		WillReturnRows(rows)
 
-	mockConn.ExpectClose()
-
-	result := GetLeastRecentPostedQuote(mockConn)
+	dbService := &DbService{DbConn: mockConn}
+	result := dbService.GetLeastRecentPostedQuote()
 
 	if err := mockConn.ExpectationsWereMet(); err != nil {
 		t.Errorf("there were unfulfilled expectations: %s", err)
 	}
 
 	assert.Equal(t, result, &Quote{ID: 1, Text: "tweet quote", Author: "Fabio"})
+}
+
+func TestUpdatePostDate(t *testing.T) {
+	mockConn, err := pgxmock.NewConn()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer mockConn.Close(context.Background())
+
+	currentTime := time.Now()
+
+	mockConn.
+		ExpectQuery("^update quotes").
+		WithArgs(currentTime, 1)
+
+	dbService := &DbService{DbConn: mockConn}
+	dbService.UpdatePostDate(1, currentTime)
+
+	if err := mockConn.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
