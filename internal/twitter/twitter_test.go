d7415f3b0f9e4041ba3096c21254c9d63384b1fe
Fabio Hiroki <fabiothiroki@gmail.com>
2021-07-06 11:34:00 -0300

    test error ase

--- internal/twitter/twitter_test.go
+++ internal/twitter/twitter_test.go
@@ -1,10 +1,12 @@
 package twitter
 
 import (
+	"errors"
 	"net/http"
 	"testing"
 
 	"github.com/dghubble/go-twitter/twitter"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -15,16 +17,31 @@ type mockStatusService struct {
 func (m *mockStatusService) Update(status string, params *twitter.StatusUpdateParams) (*twitter.Tweet, *http.Response, error) {
 	args := m.Called(status, params)
 
-	return nil, &http.Response{StatusCode: 200}, args.Error(2)
+	return args.Get(0).(*twitter.Tweet), args.Get(1).(*http.Response), args.Error(2)
 }
 
 func TestPostTweetStatusUpdate(t *testing.T) {
 	var params *twitter.StatusUpdateParams
+	postedTweet := &twitter.Tweet{ID: 1234}
 
 	mockService := new(mockStatusService)
-	mockService.On("Update", "tweeted quote", params).Return(nil, &http.Response{StatusCode: 200}, nil)
+	mockService.On("Update", "tweeted quote", params).Return(postedTweet, &http.Response{StatusCode: 200}, nil)
 
-	PostTweetStatusUpdate(mockService, "tweeted quote")
+	result, _ := PostTweetStatusUpdate(mockService, "tweeted quote")
 
 	mockService.AssertExpectations(t)
+	assert.Equal(t, result, postedTweet)
+}
+
+func TestPostTweetStatusUpdateError(t *testing.T) {
+	var params *twitter.StatusUpdateParams
+	postedTweet := &twitter.Tweet{ID: 1234}
+
+	mockService := new(mockStatusService)
+	mockService.On("Update", "tweeted quote", params).Return(postedTweet, &http.Response{StatusCode: 200}, errors.New("error"))
+
+	defer func() { recover() }()
+	PostTweetStatusUpdate(mockService, "tweeted quote")
+
+	t.Errorf("did not panic")
 }
